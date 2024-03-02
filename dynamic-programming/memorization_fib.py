c8322eb7cbaac36cdbafcdca9ab99c6371fdb900
kalinchkma <chakma.kalin10211@gmail.com>
2024-03-02 13:53:01 +0600

    memorization fixed

--- dynamic-programming/memorization_fib.py
+++ dynamic-programming/memorization_fib.py
@@ -1,10 +1,10 @@
 memo = {0: 0, 1: 1} 
 
 def fib(n):
+    print(memo)
     if n in memo:
         return memo[n]
-
-    return fib(n-1) + fib(n-2)
-
+    memo[n] = fib(n-1) + fib(n-2)
+    return memo[n]
 print(fib(10))
 
