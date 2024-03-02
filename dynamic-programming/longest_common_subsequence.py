efad48e98317e6b17ed03ad8d251c2de43e7efb5
kalinchkma <chakma.kalin10211@gmail.com>
2024-03-03 01:47:56 +0600

    longest common subsequence implemented

--- dynamic-programming/longest_common_subsequence.py
+++ dynamic-programming/longest_common_subsequence.py
@@ -1,8 +1,10 @@
+import numpy as np
+
 def longest_common_subsequence(str1, str2):
     m = len(str1)
     n = len(str2)
 
-    dp = [[0] * (n + 1) for _ in range(m+1)]
+    dp = np.array([[0] * (n + 1) for _ in range(m+1)])
     
     for i in range(1, m + 1):
         for j in range(1, n + 1):
@@ -10,8 +12,9 @@ def longest_common_subsequence(str1, str2):
                 dp[i][j] = dp[i-1][j -1] + 1
             else:
                 dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
-
+        print(str1[i-1], "->\n",dp)
+    print("Final:",dp)
     return dp[m][n]
 
 
-print(longest_common_subsequence("GXTXAYB", "AGGTAB"))
+print(longest_common_subsequence("GXTXAYB", "AGGTA"))
